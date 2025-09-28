09cae553a7f956cc341c39d780ce3a88c570352e
WeichunAuto <seven.wangweichun@outlook.com>
2025-09-28 13:02:47 +1300

    adjust route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -4,8 +4,5 @@ pub mod user;
 
 pub fn create_routes() -> Router {
     // 所有 query_user 相关路由放在 /query_user 下
-    Router::new()
-        .nest("/query_user", user::query_routes())
-        .nest("/create_user", user::create_routes())
-        .nest("/query_user_error", user::error_routes())
+    Router::new().nest("/api", user::routes())
 }
--- src/routes/user.rs
+++ src/routes/user.rs
@@ -5,14 +5,9 @@ use axum::{
 
 use crate::handlers::user;
 
-pub fn create_routes() -> Router {
-    Router::new().route("/", post(user::create_user))
-}
-pub fn query_routes() -> Router {
-    // 访问路径： /query_user/44/bobby
-    Router::new().route("/{id}/{name}", get(user::get_user))
-}
-
-pub fn error_routes() -> Router {
-    Router::new().route("/{id}/{name}", get(user::get_error))
+pub fn routes() -> Router {
+    Router::new()
+        .route("/create_user", post(user::create_user))
+        .route("/query_user/{id}/{name}", get(user::get_user))
+        .route("/query_user_error/{id}/{name}", get(user::get_error))
 }
